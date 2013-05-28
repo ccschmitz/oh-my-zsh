0289e0f3a25dbba1796f16a2a9732f1c444b3f72
Chris Schmitz <ccschmitz@gmail.com>
2013-05-28 08:41:44 -0500

    Setup Redis, Mac audio reset, Postgres and a Git alias

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,4 +2,15 @@
 alias g="git"
 alias gs="git status"
 alias gc="git commit"
+alias gcv="git commit -v"
 alias gA"git add -A"
+
+# Reset Coreaudio service
+alias resetaudio="sudo kill -9 `ps ax|grep 'coreaudio[a-z]' | awk '{print $1}'`"
+
+# Redis
+alias startredis="redis-server /usr/local/etc/redis.conf"
+
+# PostgreSQL
+alias pgstop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
+alias pgstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
